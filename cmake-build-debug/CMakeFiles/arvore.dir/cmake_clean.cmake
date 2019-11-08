6878390ddc10f86bc39a88ec02701a4161614382
sikkex <daniel_mattos@id.uff.br>
2019-11-08 19:39:15 -0300

    iniciando o projeto

---
+++ cmake-build-debug/CMakeFiles/arvore.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arvore.dir/main.c.o"
+  "arvore"
+  "arvore.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arvore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
