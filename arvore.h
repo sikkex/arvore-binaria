29eb4fd2348765d5f8b284b0938532c3a38bea56
sikkex <daniel_mattos@id.uff.br>
2019-11-08 22:31:23 -0300

    update arvore.h

--- arvore.h
+++ arvore.h
@@ -17,11 +17,7 @@ struct arv {
 
 ArvNo* arv_criano (char c, ArvNo* esq, ArvNo* dir);
 Arv* arv_cria (ArvNo* r);
-static void imprime (ArvNo* r, int tab);
 void arv_imprime (Arv* a);
-static void libera (ArvNo* r);
 void arv_libera (Arv* a);
-static int pertence (ArvNo* r, char c);
 int arv_pertence (Arv* a, char c);
-static ArvNo* busca (ArvNo* r, char c);
 ArvNo* arv_busca (Arv* a, char c);
